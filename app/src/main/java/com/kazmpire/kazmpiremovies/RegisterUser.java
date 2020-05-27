0a323433e30d1faef70ea82db86252430e0be56c
Emeka Chukwulobe <emekabe@yahoo.com>
2020-05-27 10:09:28 +0100

    Finished the first version of username validator. Still need to make some more changes

--- app/src/main/java/com/kazmpire/kazmpiremovies/RegisterUser.java
+++ app/src/main/java/com/kazmpire/kazmpiremovies/RegisterUser.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,7 +18,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class RegisterUser extends AppCompatActivity {
 
@@ -27,6 +34,8 @@ public class RegisterUser extends AppCompatActivity {
     EditText reenterPasswordField;
 
     Button submitButton;
+
+    ProgressBar progressBar;
 
 //    FirebaseDatabase database;
 //    DatabaseReference dbReference;
@@ -47,28 +56,57 @@ public class RegisterUser extends AppCompatActivity {
 
         submitButton = findViewById(R.id.submit_button);
 
+        progressBar = findViewById(R.id.progress_bar);
 
+        progressBar.setVisibility(View.GONE);
 
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                progressBar.setVisibility(View.VISIBLE);
+
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null){
-                    String firstName = firstNameField.getText().toString().trim();
-                    String lastName = lastNameField.getText().toString().trim();
-                    String userName = usernameField.getText().toString().trim();
-                    String password = passwordField.getText().toString();
+                    final String firstName = firstNameField.getText().toString().trim();
+                    final String lastName = lastNameField.getText().toString().trim();
+                    final String userName = usernameField.getText().toString().trim();
+                    final String password = passwordField.getText().toString();
                     String reenterPassword = reenterPasswordField.getText().toString();
 
-                    DatabaseReference dbReference = FirebaseDatabase.getInstance().getReference("users");
+                    final DatabaseReference dbReference = FirebaseDatabase.getInstance().getReference("users");
 
                     if (!(firstName.isEmpty() || userName.isEmpty() || password.isEmpty() || reenterPassword.isEmpty())){
-                        if (passWordValidator(password, reenterPassword)){
-                            User newUser = new User(user.getUid(), firstName, lastName, userName, password);
-                            dbReference.child(user.getUid()).setValue(newUser);
-                            startActivity(new Intent(RegisterUser.this, FactsActivity.class));
-                            finish();
+                        if (usernameValidator(userName) && passWordValidator(password, reenterPassword)){
+
+                            Query q = dbReference.orderByChild("username").equalTo(userName);
+
+                            q.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                    // TODO 1: Toast this later
+//                                    dataSnapshot.getChildren().iterator().next().getValue(User.class).getUsername().toLowerCase();
+
+                                    if (dataSnapshot.exists()){
+                                        Toast.makeText(RegisterUser.this, "Username already taken", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                    else {
+                                        User newUser = new User(user.getUid(), firstName, lastName, userName, password);
+                                        dbReference.child(user.getUid()).setValue(newUser);
+                                        progressBar.setVisibility(View.GONE);
+                                        startActivity(new Intent(RegisterUser.this, FactsActivity.class));
+                                        finish();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
                         }
                     }
                     else {
@@ -96,13 +134,36 @@ public class RegisterUser extends AppCompatActivity {
         return true;
     }
 
-    private boolean usernameValidator(String username){
+    private boolean usernameValidator(final String username){
 
         if (username.contains(" ")){
             Toast.makeText(RegisterUser.this, "Can't have spaces in username", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        return true;
+        final Boolean[] validate = {true};
+
+//        Query un = FirebaseDatabase.getInstance().getReference("users").orderByChild("username").equalTo(username);
+//
+//        un.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+//                    if (username.toLowerCase().equals(Objects.requireNonNull(ds.getValue(User.class)).getUsername().toLowerCase())){
+//                        //Wowwwww.. Let's hope this works
+//                        validate[0] = false;
+//                        Toast.makeText(RegisterUser.this, "Username already taken", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+        return validate[0];
     }
 }
